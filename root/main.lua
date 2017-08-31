8a902794b19db120ca46737b45ddccc722d689ce
David Corticchiato <david.corticchiato@gmail.com>
2017-08-31 12:28:35 +0200

    load relative script

--- root/main.lua
+++ root/main.lua
@@ -1,9 +1,25 @@
-function split(filename)
-	return string.match(filename, "(.-)([^/]-([^/%.]+))$")
-end
+local path = require('pl.path')
 
-path,file,extension = split(os.getenv("CRUSH_MAIN"))
+local input_path = os.getenv('CRUSH_MAIN')
+local current_dir = os.getenv('CRUSH_PWD')
 
-package.path = package.path .. path
 
-require(file)
+local full_path = path.abspath(input_path, current_dir)
+local dirname, filename = path.splitpath(full_path)
+local basename = path.splitext(filename)
+
+package.path = package.path .. ";" .. dirname .. "/?.lua"
+
+require(basename)
+
+-- _G.pcall(_G.require, basename)
+
+-- function split(filename)
+-- 	return string.match(filename, '(.-)([^/]-([^/%.]+))$")
+-- end
+--
+-- path,file,extension = split(os.getenv("CRUSH_MAIN"))
+--
+--
+--
+-- require(file)
